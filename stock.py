3a55d3cb6cc65962a7bf7aa241364b99cd1ce84d
ztyh <ztyh0121@gmail.com>
2019-02-25 18:31:31 -0500

    Create stock.py
    reads quotes

---
+++ stock.py
@@ -0,0 +1,47 @@
+import subprocess
+import json
+
+
+def getquote(stock):
+    command = 'curl -v https://api.robinhood.com/quotes/' + \
+        stock + '/ -H "Accept: application/json"'
+    p = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    quote = json.loads(out.decode("utf-8"))
+    return quote
+
+
+stock = 'SPY'
+
+
+def getfdmtl(stock):
+    command = 'curl -v https://api.robinhood.com/fundamentals/' + \
+        stock + '/ -H "Accept: application/json"'
+    p = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    fdmtl = json.loads(out.decode("utf-8"))
+    return fdmtl
+
+
+getfdmtl('SPY')
+
+
+def getmkt():
+    command = 'curl -v https://api.robinhood.com/markets/ / -H "Accept: application/json"'
+    p = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    mkt = json.loads(out.decode("utf-8"))
+    return mkt['results']
+
+
+markets = getmkt()
+len(markets)
+markets[0]
+markets[1]
+markets[2]
+markets[3]
+markets[4]
+markets[5]
